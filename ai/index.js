6ea2f4169f7b50e8d51460b56ee1bd850a55a110
Chris odhiambo <122823108+chriis-fr@users.noreply.github.com>
2023-11-25 00:11:50 +0300

    Add files via upload

---
+++ ai/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const {Configuration, OpenAIApi} = require('openai')
+
+
+app.use(express.static('public'))
+
+app.listen(5000, ()=> {
+    console.log('server is active')
+})
+
+const openai = new OpenAIApi(new Configuration({
+    apiKey: "your_api_key"
+}))
+
+app.post('/chat', async (req, res)=> {
+    try{
+        const resp = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: [
+                {role: 'user', content: req.body.question}
+            ]
+        })
+        res.status(200).json({message: resp.data.choices[0].message.content})
+    } catch(e){
+        res.status(400).json({message: e.message})
+    }
+})
